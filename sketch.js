6c6cb95dc50c14b45af9f98001f9ecf701531a33
Matthew <stovianii@gmail.com>
2019-02-10 14:51:29 -0600

    Began Genetic Algorithm but currently having issues due to my lack of experience with scope in Javascript

--- sketch.js
+++ sketch.js
@@ -1,32 +1,56 @@
 let p;
+let goal;
+//Standard p5.js starts with a setup, where "preprocessing" begins
 function setup() {
-    createCanvas(640, 640);
+    createCanvas(800, 800);
     background(0);
-    p = new Population(100);
+    //Initializes a population of dots (exact number is the parameter value)
+    p = new Population(1000);
+    //The goal to "win" the game is for the points to hit the circle as the goal vector
+    goal = createVector(400,100);
 }
 
+//Standard p5.js starts with a draw function which loops every frame
+//Everything within this function will occur over in over as if it's in a loop
 function draw(){
-    background(0);
-    p.update();
-    p.show();
+    
+    if(p.checkAllDead())
+    {
+        p.getFitness();
+        p.naturalSelection();
+    } 
+    else 
+    {
+        background(0);
+        fill(255,255,0);
+        ellipse(goal.x,goal.y,10,10);
+        //updates the position of every dot and shows it on the canvas
+        p.update();
+        p.show();
+    }
 }
 
-//-----------------------------------------------------------------------------------------------------
+
+/////////////////DOT CLASS///////////////////
+
 class dot {
     constructor(size){
-        this.pos = createVector(width/2,height/2);
+        this.pos = createVector(width/2,height-15);
         this.vel = createVector(0,0);
         this.acc = createVector(0,0);
         this.dead = false;
+        this.success = false;
         this.size = size;
         this.b = new Brain(this.size);
     }
 
+    //shows the current position of the dot
     show() {
         fill(255,0,0);
         ellipse(this.pos.x,this.pos.y,5,5);
     }
 
+    //moves the dot's position based on its acceleration, velocity, and position
     move() {
         if(this.size>this.b.step)
         {
@@ -42,20 +66,39 @@ class dot {
         this.pos.add(this.vel);
     }
 
+    //determines if the dot has hit a wall or hit objective and calls the move function
     update() {
-        if(!this.dead)
+        if(!this.dead && !this.success)
         {
             this.move();
             if(this.pos.x<4 || this.pos.y<4 || this.pos.x > width - 4 || this.pos.y > height-4)
             {
                 this.dead = true; 
             }
+            else if (dist(this.pos.x,this.pos.y,goal.x,goal.y)<7)
+            {
+                this.success = true;
+            }
         }
-        return this.dead;
+    }
+
+    //Calculates how well a dot did at the game based on how close it is to the goal after the game ends
+    getFitness(){
+        this.distance = dist(this.pos.x,this.pos.y,goal.x,goal.y);
+        this.fitness = 1/this.distance;
+    }
+
+    //Dot reproduces for next generation
+    reproduce(){
+        print("TEST2");
+        let baby = new dot();
+        baby.b = this.b.clone();
+
+        return baby;
     }
 }
 
-//----------------------------------------------------------------------------------------------------------------
+/////////////////BRAIN CLASS///////////////////
 
 class Brain {
     constructor(size){
@@ -65,39 +108,109 @@ class Brain {
         this.randoming();
     }
 
+    //generates random directions for each dot at generation 1
     randoming(){
 
-        for(this.i=0;this.i<this.size;this.i++)
+        for(let i=0;i<this.size;i++)
         {
             let randomAngle = random(2*PI);
-            this.directions[this.i] = p5.Vector.fromAngle(randomAngle);
+            this.directions[i] = p5.Vector.fromAngle(randomAngle);
+        }
+    }
+    //clones the directions of the parent to the reproduced baby
+    clone(){
+        let clone = new Brain(this.size);
+        for(let i=0;i<this.size;i++)
+        {
+            clone.directions[i] = directions[i];
         }
     }
 };
 
-//-------------------------------------------------------------------------------
+/////////////////POPULATION CLASS///////////////////
 
 class Population {
     constructor(size){
         this.d = new dot();
         this.size = size;
-        for(this.i=0;this.i<size;this.i++)
+        this.fitnessSum = 0;
+        this.generation = 1;
+        for(let i=0;i<size;i++)
         {
-            this.d[this.i] = new dot(400);
+            this.d[i] = new dot(400);
         }
     }
 
     show(){
-        for(this.i=0;this.i<this.size;this.i++)
+        for(let i=0;i<this.size;i++)
         {
-            this.d[this.i].show();
+            this.d[i].show();
         }
     }
 
     update(){
-        for(this.i=0;this.i<this.size;this.i++)
+        for(let i=0;i<this.size;i++)
         {
-            this.d[this.i].update();
+            this.d[i].update();
+        }
+    }
+
+    getFitness(){
+        for(let i=0;i<this.size;i++)
+        {
+            this.d[i].getFitness();
+        }
+    }
+
+    checkAllDead(){
+        for(let i=0;i<this.size;i++)
+        {
+            if(!this.d[i].dead)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    naturalSelection(){
+        let d2 = new dot();
+        let parent = new dot();
+        this.calculateFitnessSum();
+        print(this.fitnessSum);
+        for(let i=0;i<this.size;i++)
+        {
+            //determine parents that will breed next genertation
+            parent = this.determineParent();
+            d2[i] = parent.reproduce();
+        }
+
+        this.d = d2.clone();
+        this.generation++;
+    }
+
+    calculateFitnessSum(){
+         //calculates the sum of all the dot's fitness
+         for(let i=0;i<this.size;i++)
+         {
+             this.fitnessSum += this.d[i].getFitness();
+         }
+    }
+
+    determineParent(){
+        //Chooses which "parent zone" to be used (look up Code Bullet video for more details)
+        let rNum = random(this.fitnessSum);
+        //Picks the parent that will breed based of rNum (most likely to be higher fitness parents due to them dominating fitnessSum zone)
+        let dotSum = 0;
+        for(let i=0;i<this.size;i++)
+        {
+            //print(rNum + " " + dotSum);
+            this.dotSum += this.d[i].getFitness();
+            if(rNum > dotSum)
+            {
+                print("test3");
+                return this.d[i];
+            }
         }
     }
 };
